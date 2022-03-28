99011f1057ac8bd41e243c251d42d790e9032812
pakcyeong <p1105_00@naver.com>
2022-03-29 05:38:40 +0900

    rough sketch
    simple darkmode on web

---
+++ src/script.js
@@ -0,0 +1,9 @@
+/* dark mode on/off */
+function dark(){
+    var target = document.body;
+    target.classList.toggle("dark-mode");
+}
+
+/*
+*   button onclick -> body, all of font, change their color. also text of button change sun <-> moon
+*/
